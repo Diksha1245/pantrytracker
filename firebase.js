32d53e7e1855a1f6f4668e1dc72350a6f7fa2c43
Diksha Kumaraguru <90680492+Diksha1245@users.noreply.github.com>
2024-08-09 21:08:28 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR-API-KEY",
-  authDomain: "hspantryapp-1a8d3.firebaseapp.com",
-  projectId: "hspantryapp-1a8d3",
-  storageBucket: "hspantryapp-1a8d3.appspot.com",
-  messagingSenderId: "282059438315",
-  appId: "1:282059438315:web:52dc22d17e92bcda6466ae"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
